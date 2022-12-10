7021818150593e83cd733e3a0d7de2cd6e88a204
8D2D <74885040+Dylan23145@users.noreply.github.com>
2022-12-10 00:21:01 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ mv asis /data/data/com.termux/files/usr/bin/
 cd /data/data/com.termux/files/usr/bin/
 chmod +x asis 
 cd $HOME
-tput 10;echo 'Asis ha sido instalado en tu terminal
+rm -rf Asis
+tput setaf 10;echo 'Asis ha sido instalado en tu terminal
 
 Para poder ejecutarlo en un futuro escribe
 
