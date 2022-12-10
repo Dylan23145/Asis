6b40ec106e8cc6bd79f592eaf2b0100b1f63d843
8D2D <74885040+Dylan23145@users.noreply.github.com>
2022-12-10 00:19:24 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,9 @@ pkg install jq -y
 mv asis /data/data/com.termux/files/usr/bin/
 cd /data/data/com.termux/files/usr/bin/
 chmod +x asis 
+cd $HOME
+tput 10;echo 'Asis ha sido instalado en tu terminal
+
+Para poder ejecutarlo en un futuro escribe
+
+asis "pregunta" inténtalo! '
